724587c1467fad8cf13f0acf6867e0636dcacd83
Franco Castro <franco.castro.villanueva.90@gmail.com>
2025-05-11 15:40:13 -0400

    config

--- src/main/java/com/duoc/authService/AuthServiceApplication.java
+++ src/main/java/com/duoc/authService/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package duoc.auth_service;
+package com.duoc.authService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/duoc/authService/AuthServiceApplicationTests.java
+++ src/test/java/com/duoc/authService/AuthServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package duoc.auth_service;
+package com.duoc.authService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
